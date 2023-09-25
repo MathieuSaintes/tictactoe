0960ece584cb0aa01a563cbe64bab12e8bbc46ed
MathieuSaintes <mathieu.saintes@gmail.com>
2023-09-25 14:31:36 +0800

    Added choice of level
    Rewrote the button class
    Rewrote the display_message function (now blit_text)

--- Graphics.py
+++ Graphics.py
@@ -2,18 +2,25 @@ import itertools, random, pygame
 from AI import *
 pygame.init()
 
-class Button(pygame.Rect):
-    def __init__(self, text, color, width, height, bgcolor, center):
-        super().__init__(0, 0, width, height)
-        self.text = text
-        self.center = center
-        self.text = LARGE_FONT.render(text, True, color)
-        self.bgcolor = bgcolor
+MEDIUM_FONT = pygame.font.Font(None, 30)
+LARGE_FONT = pygame.font.Font(None, 60)
 
+def blit_text(text, color, font, center):
+    text_surface = font.render(text, True, color)
+    DIS.blit(text_surface, text_surface.get_rect(center=center))
+
+class Button(pygame.Rect):
+    def __init__(self, content, color, font, bgcolor, width, height, center):
+        self.content = content
+        self.color = color
+        self.font = font
+        self.bgcolor = bgcolor
+        super().__init__(0, 0, width, height)
+        self.center = center
     def draw(self):
         DIS.fill(self.bgcolor, self)
         pygame.draw.rect(DIS, 'black', self, width=1)
-        DIS.blit(self.text, self.text.get_rect(center=self.center))
+        blit_text(self.content.upper(), self.color, self.font, self.center)
 
 
 def draw_cell(action, color, sign):
@@ -25,28 +32,30 @@ def draw_cell(action, color, sign):
         pygame.draw.line(DIS, color, DRAWING_AREA.topright, DRAWING_AREA.bottomleft)
 
 
-def initialization():
-
-    global player1, player2, pool, current, other
-
-    # Choose Symbol
+def select_button(buttons):
     DIS.fill('grey')
-    X_button.draw()
-    O_button.draw()
+    for button in buttons:
+        button.draw()
     pygame.display.update()
     while True:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN and X_button.collidepoint(event.pos):
-            HUMAN_SIGN = 'X'
-            AI_SIGN = 'O'
-            break
-        elif event.type == pygame.MOUSEBUTTONDOWN and O_button.collidepoint(event.pos):
-            HUMAN_SIGN = 'O'
-            AI_SIGN = 'X'
-            break
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for button in buttons:
+                if button.collidepoint(event.pos):
+                    return button.content
 
+
+def initialization():
+
+    global player1, player2, pool, current, other, level
+
+    # Choose sign and level
+    HUMAN_SIGN = select_button(sign_buttons)
+    AI_SIGN = {'X': 'O', 'O': 'X'}[HUMAN_SIGN]
+    level = select_button(level_buttons)
+    
     # Initialize Variables
     player1 = { 'id': 1, 
                 'sign': AI_SIGN, 
@@ -68,14 +77,6 @@ def initialization():
     pygame.display.update()
 
 
-
-FONT = pygame.font.Font(None, 30)
-LARGE_FONT = pygame.font.Font(None, 60)
-def display_message(message, color):
-    message_surface = FONT.render(message, True, color)
-    message_rectangle = message_surface.get_rect()
-    message_rectangle.center = DIS.get_rect().center
-    DIS.blit(message_surface, message_rectangle)
 
 
 # Dimensions
@@ -99,9 +100,20 @@ for x,y in itertools.product(range(3), repeat=2):
 INT_BD = 1 * BLK
 DRAWING_AREA = pygame.Rect(0, 0, CELL_WIDTH - 2 * INT_BD, CELL_WIDTH - 2 * INT_BD)
 
-# Setup of the 2 buttons
-X_button = Button('X', 'black', 8*BLK, 8*BLK, 'white', pygame.Rect((0, 0, DIS_WIDTH//2, DIS_WIDTH)).center)
-O_button = Button('O', 'black', 8*BLK, 8*BLK, 'white', pygame.Rect((DIS_WIDTH//2, 0, DIS_WIDTH//2, DIS_WIDTH)).center)
+
+###  Creation of the sign and level buttons  ###
+
+signs = ('X', 'O')
+sign_buttons = []
+for i, sign in enumerate(signs):
+    sign_buttons.append( Button(sign, 'black', LARGE_FONT, bgcolor='white', width=8*BLK, height=8*BLK, center=((1+2*i)*DIS_WIDTH//4 , DIS_WIDTH//2)) )
+
+levels = ('easy', 'medium', 'hard')
+level_buttons = []
+for i, level in enumerate(levels):
+    level_buttons.append( Button(level, 'black', MEDIUM_FONT, bgcolor='white', width=20*BLK, height=8*BLK, center=(DIS_WIDTH//2 , (i+1)*DIS_WIDTH//4)) )
+
+
 
 # Setup of the game clock and game speed
 FPS = 10
@@ -129,7 +141,7 @@ while True:
                         break
     
     elif current['type'] == 'AI':
-            action_to_play = get_AI(current, other, pool, 'hard')
+            action_to_play = get_AI(current, other, pool, level)
 
 
     # Play the chosen action 
@@ -144,24 +156,22 @@ while True:
 
         if winning_streak:
 
-            print('winning_streak:', winning_streak)
-
             if current['type'] == 'AI':
                 message = 'Computer wins'
                 color = 'red'
             else:
                 message = 'You win!'
                 color = 'green' 
-            display_message(message, color)
             
             for action in winning_streak:
                 draw_cell(action, color, current['sign'])
 
-
         elif len(pool) == 0:
-            print('draw')
-            display_message('Draw', 'blue')
 
+            message = 'Draw'
+            color = 'blue'
+
+        blit_text(message, color, MEDIUM_FONT, center=DIS.get_rect().center)
         clock.tick(FPS) #needs to be added before the display.update for the update to appear on the screen
         pygame.display.update()
 
